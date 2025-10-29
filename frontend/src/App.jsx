4ee4706f7de6e4114ccc803af14ca1369f643969
DaMike7 <michaeldabere462@gmail.com>
2025-10-29 16:48:29 +0100

    initialized the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-red-500'>
+        Testing
+      </p>
+    </>
+  )
+}
+
+export default App
